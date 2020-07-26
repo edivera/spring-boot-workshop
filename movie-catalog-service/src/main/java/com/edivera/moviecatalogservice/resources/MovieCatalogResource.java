36beb00f1cd2535b8081460dc9c031997e0f9763
Edilio Vera <ediliovera97@gmail.com>
2020-07-26 15:03:20 -0500

    added movie and rating models to movie catalog service to obtain models from the other rest resources in the other two microservices

--- movie-catalog-service/src/main/java/com/edivera/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/edivera/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,12 +1,17 @@
 package com.edivera.moviecatalogservice.resources;
 
 import com.edivera.moviecatalogservice.models.CatalogItem;
+import com.edivera.moviecatalogservice.models.Movie;
+import com.edivera.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController     // marks the class as request handler
 @RequestMapping("/catalog") // mapping for any request. could have been added inside as well
@@ -19,8 +24,32 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCalaog(@PathVariable("userId") String userId) {
         // maps /catalog/{userId} to this function. all well. part of most backends
 
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Test", 4)
+        RestTemplate restTemplate = new RestTemplate(); // creates a call to another microservice
+        // makes a rest call to any URL and gets back a json string response.
+        // if you know what the payload is on its return, pass it, it creates an object and
+        // populates the fields and returns that object.
+        // it's also a GET http method caller.
+
+
+
+        // get all rated movie IDs
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
         );
+
+        return ratings.stream().map(rating -> {
+            // For each movie ID, call movie info service and get details
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"
+                            + rating.getMovieId(), Movie.class);
+
+            // put them all together
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
+
+        // previous hardcorded catalogitem list
+//        return Collections.singletonList(
+//                new CatalogItem("Transformers", "Test", 4)
+//        );
     }
 }
