a4f00ddd780566dd033271615e1f5cec9772de05
Edilio Vera <ediliovera97@gmail.com>
2020-07-26 15:45:01 -0500

    investigated webclient alternative

--- movie-catalog-service/src/main/java/com/edivera/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/edivera/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+
+	@Bean	// bean for webclient
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
+	}
 
 	@Bean	// producer, executes method and stores instance somewhere
 	public RestTemplate getRestTemplate() {
--- movie-catalog-service/src/main/java/com/edivera/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/edivera/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -20,7 +21,10 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient.Builder webClientBuilder; // beaned up
+
+    @Autowired
+    private RestTemplate restTemplate;  // beaned up
 
     @RequestMapping("/{userId}")    // variable userId
     // could have also been @RequestMapping("/catalog/{userId}").
@@ -35,14 +39,30 @@ public class MovieCatalogResource {
         // populates the fields and returns that object.
         // it's also a GET http method caller.
 
-
-
         // get all rated movie IDs
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234", 4),
                 new Rating("5678", 3)
         );
 
+        // using WebClient, you need the Spring Reactive Web dependency
+        // tell webclient to go do the calls and then give it callback to do something
+        //WebClient.Builder webClientBuilder = WebClient.builder();  // template builder pattern
+        // beaned up
+
+        return ratings.stream().map(rating -> {
+            Movie movie = webClientBuilder.build()
+                    .get()  // get method
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId()) // api route
+                    .retrieve() // fetch data
+                    .bodyToMono(Movie.class)    // change json to object, mono promise
+                    .block();   // block the current thread until request is done
+
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());    // collects the results of the map as a list
+        // a map is really just a fancy for each loop using a lamda function
+
+        /*
         return ratings.stream().map(rating -> {
             // For each movie ID, call movie info service and get details
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"
@@ -51,6 +71,7 @@ public class MovieCatalogResource {
             // put them all together
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         }).collect(Collectors.toList());
+        */
 
         // previous hardcorded catalogitem list
 //        return Collections.singletonList(
